e8b44bb884b2ab89509d371db3fc802df0f489b3
Julia Reed <juliareed@Julias-iMac.local>
2025-05-24 14:42:28 -0500

    add body footer header components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-root', // The selector is used to identify this component in the HTML
+  standalone: true, // This component is standalone, meaning it can be used without being declared in an NgModule
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';   
+import { FooterComponent } from './footer/footer.component';
+import { BodyComponent } from './body/body.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'body', component: BodyComponent }
+   
+];
