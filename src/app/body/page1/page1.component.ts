ed72055f6993f1249e8b7ec6fa604e861612a065
aimebyiringiro <byiringiroaimeismael@gmail.com>
2025-05-27 22:33:57 -0500

    general layout with sections of pages

--- src/app/body/page1/page1.component.ts
+++ src/app/body/page1/page1.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { VantaBackgroundComponent } from './vantabackground/vantabackground.component';
 
 @Component({
   selector: 'app-page1',
-  imports: [],
+  imports: [VantaBackgroundComponent],
   templateUrl: './page1.component.html',
   styleUrl: './page1.component.css'
 })
