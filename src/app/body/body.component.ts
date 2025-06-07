61c28a452469d67bf2763c230eb9ad2e1eea5372
aimebyiringiro <byiringiroaimeismael@gmail.com>
2025-06-06 23:02:47 -0500

    wanting to clearn commit

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component} from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-body',
   imports: [
     Page1Component,
     Page2Component,
-    Page3Component
+    Page3Component,
+    ArticlesComponent,
+    CarouselComponent
   ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
--- src/app/body/page2/page2.component.ts
+++ src/app/body/page2/page2.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'; 
+
 
 @Component({
   selector: 'app-page2',
@@ -7,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrl: './page2.component.css'
 })
 export class Page2Component {
+
+  articleUrl: SafeResourceUrl;
+
+  constructor (private sanitizer: DomSanitizer) {
+    this.articleUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://en.wikipedia.org/wiki/AI_boom');
+  }
+
+
 
 }
