991af896b56ecdd1cfa3bb51fb383478efc827c7
aimebyiringiro <byiringiroaimeismael@gmail.com>
2025-06-06 21:44:20 -0500

    added a video

--- src/app/body/page3/page3.component.ts
+++ src/app/body/page3/page3.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { YouTubePlayer } from '@angular/youtube-player';
 
 @Component({
   selector: 'app-page3',
-  imports: [],
+  imports: [
+    YouTubePlayer
+  ],
   templateUrl: './page3.component.html',
   styleUrl: './page3.component.css'
 })
+
+
 export class Page3Component {
 
+
+
 }
+// https://github.com/angular/components/blob/main/src/youtube-player/README.md
+// https://www.youtube.com/watch?v=MoxFkJlVZlA
