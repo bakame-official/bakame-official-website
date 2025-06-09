6a66a62b11502eadde77c5da09f866455ee2c2f5
aimebyiringiro <byiringiroaimeismael@gmail.com>
2025-06-08 20:12:04 -0500

    working on mobile menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    CommonModule
+   
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  isMenuOpen = false; // Variable to track menu state
+
+  toggleMenu() {  // Function to toggle menu, to toggle means open and close, it returns the opposite of the current state
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
 
 }
