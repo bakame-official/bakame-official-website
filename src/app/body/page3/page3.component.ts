d399d588577ebec87a67f18f6e3672db8fa7a3aa
aimebyiringiro <byiringiroaimeismael@gmail.com>
2025-06-16 12:26:35 -0500

    chaning the frame

--- src/app/body/page3/page3.component.ts
+++ src/app/body/page3/page3.component.ts
@@ -3,18 +3,9 @@ import { YouTubePlayer } from '@angular/youtube-player';
 
 @Component({
   selector: 'app-page3',
-  imports: [
-    YouTubePlayer
-  ],
+  standalone: true,
+  imports: [YouTubePlayer],
   templateUrl: './page3.component.html',
   styleUrl: './page3.component.css'
 })
-
-
-export class Page3Component {
-
-
-
-}
-// https://github.com/angular/components/blob/main/src/youtube-player/README.md
-// https://www.youtube.com/watch?v=MoxFkJlVZlA
+export class Page3Component {}
